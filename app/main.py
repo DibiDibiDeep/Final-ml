4a5694b50551ac2641767acf277e161699779675
okchang95 <okchang95@gmail.com>
2024-09-25 14:23:12 +0900

    update: origin 수정

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ from app.api.fairytale import fairytale
 
 import os
 
-origins = [os.getenv("DOCKER_BRIDGE_BACK")]
+# origins = [os.getenv("DOCKER_BRIDGE_BACK")]
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
