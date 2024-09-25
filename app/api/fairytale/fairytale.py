71cc4026fcb860ef84858139a33aede9501d7d62
insukim <rladlstn494@naver.com>
2024-09-25 11:46:10 +0900

    fix:dall-e api 429 Error

--- app/api/fairytale/fairytale.py
+++ app/api/fairytale/fairytale.py
@@ -3,15 +3,13 @@ from app.api.fairytale.utils.fairytale_utils import (
     create_pairy_chain,
     save_result_to_json,
 )
-from app.api.fairytale.utils.fairytale_image_utils import generate_cover_image, generate_page_image
+from app.api.fairytale.utils.fairytale_image_utils import generate_all_images
 from app.api.fairytale.models import FairytaleInput
 
 from dotenv import load_dotenv
-from tqdm import tqdm
 import os
 import time
-import asyncio
-
+import logging
 from openai import AsyncOpenAI
 
 from fastapi import APIRouter, HTTPException
@@ -31,6 +29,8 @@ async def generate_fairytale(input_data: FairytaleInput):
         user_id = data["user_id"]
         baby_id = data["baby_id"]
 
+
+        # Request 데이터 전처리(필요한 데이터만 추출)
         data = select_keys_from_diary_data(data)
         # 프롬프트 로드
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -46,7 +46,7 @@ async def generate_fairytale(input_data: FairytaleInput):
         # 동화 생성 체인 생성
         chain = create_pairy_chain(prompts, data)
 
-        print("동화 생성 시작")
+        logging.info("Fairytale generation started")
         # 동화 생성
         result = chain.invoke(
             {
@@ -59,7 +59,8 @@ async def generate_fairytale(input_data: FairytaleInput):
                 "art_style": "Cartoon animated",
             }
         )
-        print("동화 생성 완료")
+        logging.info("Fairytale generation completed")
+
         # 캐릭터 정보를 문자열로 변환
         characters_info = " ".join(
             [
@@ -67,34 +68,31 @@ async def generate_fairytale(input_data: FairytaleInput):
                 for char in result.get("characters", [])
             ]
         )
-        # 이미지 생성
+        # 이미지 생성 클라이언트 생성
         client = AsyncOpenAI(api_key=apikey)
-
-        # 모든 이미지 생성 작업을 비동기로 실행
-        tasks = [generate_cover_image(client, characters_info, result["book_cover_description"])] + [generate_page_image(client, characters_info, page, i) for i, page in enumerate(result["pages"])]
-        # tqdm을 사용하여 진행 상황 표시
-        print("이미지 생성 시작")
-        all_results = []
-        for f in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc="Generating images"):
-            all_results.append(await f)
+        
+        # 이미지 생성
+        cover_image, page_images = await generate_all_images(
+            client, 
+            characters_info, 
+            result["book_cover_description"], 
+            result["pages"]
+        )
 
         # 결과 처리
-        result["title_img_path"] = all_results[0]  # 첫 번째 결과는 표지 이미지 URL
+        result["title_img_path"] = cover_image
+        result["pages"] = [{"image_url": img[1]["image_url"], **img[1]} for img in sorted(page_images, key=lambda x: x[0])]
         
-        # 페이지 결과만 필터링하고 정렬
-        page_results = [r for r in all_results[1:] if isinstance(r, tuple) and len(r) == 2]
-        result["pages"] = [page for _, page in sorted(page_results, key=lambda x: x[0])]
-        
-        end_time = time.time()
-        total_time = end_time - start_time
-        print(f"모든 이미지 생성 완료. 총 소요 시간: {total_time:.2f}초")
-        print("모든 이미지 생성 완료")
 
         result.update({"user_id": user_id, "baby_id": baby_id})
+        # 총 소요 시간 계산
+        end_time = time.time()
+        total_time = end_time - start_time
+        logging.info(f"Total time: {total_time:.2f} seconds")
 
         # JSON 파일로 결과 저장
-        save_result_to_json(result, "fairytale_result4.json")
-        print("JSON 파일 저장 완료")
+        save_result_to_json(result, "fairytale_result_fix_timeout_ver3_1.json")
+        logging.info("JSON file saved")
 
         return result
     except Exception as e:
--- app/api/fairytale/utils/fairytale_image_utils.py
+++ app/api/fairytale/utils/fairytale_image_utils.py
@@ -1,20 +1,19 @@
-from typing import List
-from openai import OpenAI
 import pyshorteners
-from app.api.fairytale.models import StoryResponse, StoryPage
-
+import logging
 import asyncio
+import time
+from requests.exceptions import RequestException
+import os
 from openai import AsyncOpenAI
 
-import requests
-from requests.exceptions import RequestException
+image_model = os.getenv("IMAGE_MODEL")
 
-
+# 이미지 생성 함수
 async def generate_image(client: AsyncOpenAI, prompt: str):
 
     # DALL-E 3 모델을 사용하여 이미지 생성
     response = await client.images.generate(
-        model="dall-e-3",
+        model= image_model,
         prompt=prompt,
         size="1024x1024",
         quality="standard",
@@ -27,21 +26,65 @@ async def generate_image(client: AsyncOpenAI, prompt: str):
 
 # 표지 이미지 생성 함수
 async def generate_cover_image(client: AsyncOpenAI, characters_info: str, book_cover_description: str):
-    print("표지 이미지 생성 시작")
-    # title_prompt = f"{characters_info} {result['book_cover_description']}"
+
+    logging.info("Title image generation started")
+
+    # 표지 이미지 생성 프롬프트 생성(캐릭터 정보 + 표지 이미지 생성 프롬프트)
     title_prompt = f"{characters_info} {book_cover_description}"
     title_img_path = await generate_image(client, title_prompt)
+    # url 짧게 변경
     title_short_url = await shorten_url(title_img_path)
-    print("표지 이미지 생성 완료")
+
+    logging.info("Title image generation completed")
+
     return title_short_url
 
 # 페이지별 이미지 생성을 비동기로 처리
 async def generate_page_image(client: AsyncOpenAI, characters_info: str, page: dict, index: int):
+
+    logging.info(f"Page image generation started for page {index}")
+
+    # 페이지 이미지 생성 프롬프트 생성(캐릭터 정보 + 페이지 이미지 생성 프롬프트)
     page_prompt = f"{characters_info}{page['illustration_prompt']}"
     image_url = await generate_image(client, page_prompt)
+    # url 짧게 변경
     short_url = await shorten_url(image_url)
+
+    logging.info(f"Page image generation completed for page {index}")
+
     return index, {"image_url": short_url, **page}
+
+# 모든 이미지(표지 + 페이지) 생성 함수
+async def generate_all_images(client: AsyncOpenAI, characters_info: str, book_cover_description: str, pages: list):
+    # 이미지 생성 시작 시간 기록
+    start_time = time.time()
     
+    # 표지 이미지 생성
+    cover_task = generate_cover_image(client, characters_info, book_cover_description)
+    
+    # 페이지 이미지 생성
+    page_tasks = [generate_page_image(client, characters_info, page, i) for i, page in enumerate(pages)]
+    
+    # 모든 태스크 실행
+    all_results = await asyncio.gather(cover_task, *page_tasks)
+    
+    # 최소 1분 보장(Too Many Requests Error 방지)
+    elapsed_time = time.time() - start_time
+    # if elapsed_time < 60:
+    #     await asyncio.sleep(60 - elapsed_time)
+    MAX_EXECUTION_TIME = 60
+    MIN_EXECUTION_TIME = 55  # 최소 실행 시간을 설정
+
+    if elapsed_time < MIN_EXECUTION_TIME:
+        await asyncio.sleep(min(MIN_EXECUTION_TIME - elapsed_time, MAX_EXECUTION_TIME - elapsed_time))
+    # 결과 정리
+    cover_image = all_results[0]
+    page_images = all_results[1:]
+    logging.info("All images generation completed")
+    return cover_image, page_images
+
+
+# 이미지 URL 단축 함수
 async def shorten_url(url, max_retries=3, timeout=5):
     s = pyshorteners.Shortener(timeout=timeout)
     
@@ -50,7 +93,7 @@ async def shorten_url(url, max_retries=3, timeout=5):
             return s.tinyurl.short(url)
         except RequestException as e:
             if attempt == max_retries - 1:
-                print(f"Failed to shorten URL after {max_retries} attempts: {e}")
+                logging.error(f"Failed to shorten URL after {max_retries} attempts: {e}")
                 return url  # 실패 시 원본 URL 반환
             await asyncio.sleep(1)  # 재시도 전 1초 대기
 
