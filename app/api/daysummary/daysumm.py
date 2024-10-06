903b4451a2cc4aeb63ef1ca8714c9e1cd073b332
in-sukim <rladlstn494@naver.com>
2024-10-06 13:19:06 +0900

    feat:Daysumamry code refactoring & conditional AgentAction, Finsh setup

--- app/api/daysummary/daysumm.py
+++ app/api/daysummary/daysumm.py
@@ -1,17 +1,12 @@
-from typing import List, Union
-from dotenv import load_dotenv
-import os
-from pydantic import BaseModel
+from typing import List
 import logging
 from uuid import uuid4
 from typing import Dict
+import json
 
 from langchain.agents.format_scratchpad.log import format_log_to_str
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain.prompts import PromptTemplate
+
 from langchain.schema import AgentAction, AgentFinish
-from langchain.tools.render import render_text_description
-from langchain_openai import ChatOpenAI
 
 from .tools import (
     retriever_assistant,
@@ -19,79 +14,64 @@ from .tools import (
     sharing_assistant,
     write_diary_assistant,
     save_diary_assistant,
+    except_situation_assistant,
 )
-from .utils.agent_util import find_tool
-
+from .utils.agent_util import find_tool, setup_agent
+from .models import Query
 from fastapi import APIRouter, HTTPException
 
-llm_model = os.getenv("LLM_MODEL")
-openai_key = os.getenv("OPENAI_API_KEY")
-# template load
-current_dir = os.path.dirname(os.path.abspath(__file__))
-prompts_dir = os.path.join(current_dir, "prompts/agent", "daysummary_react_prompt.txt")
-with open(prompts_dir, "r") as f:
-    template = f.read()
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# 사용할 도구들 정의
 tools = [
     retriever_assistant,
     cls_intent_assistant,
     sharing_assistant,
     write_diary_assistant,
     save_diary_assistant,
+    except_situation_assistant,
 ]
-
-# Prompt setting
-prompt = PromptTemplate.from_template(template)
-prompt = prompt.partial(
-    tools=render_text_description(tools),
-    tool_names=", ".join(t.name for t in tools),
-)
-
-llm = ChatOpenAI(
-    model=llm_model,
-    temperature=0,
-    openai_api_key=openai_key,
-    stop=["\nObservation", "Observation"],
-)
-agent: Union[AgentAction, AgentFinish] = prompt | llm | ReActSingleInputOutputParser()
-
-
-class Query(BaseModel):
-    baby_id: int = 1
-    user_id: int = 1
-    session_id: str = None
-    text: str
-
+# 에이전트 설정
+agent = setup_agent(tools)
 
 router = APIRouter()
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-# User session management
+# 사용자 세션 관리를 위한 딕셔너리
 user_sessions: Dict[str, List[str]] = {}
 
 
 @router.post("/process_query")
 async def process_user_query(query: Query):
-    # try:
+    # 로깅: 쿼리 처리 시작
     logger.info(f"Processing query: {query}")
+
+    # 세션 ID가 없으면 새로 생성
     if not query.session_id:
         session_id = str(uuid4())
 
+    # 쿼리에서 필요한 정보 추출
     input = query.text
     user_id = query.user_id
     baby_id = query.baby_id
     session_id = query.session_id
 
+    # 채팅 기록 가져오기 또는 새로 생성
     chat_history = user_sessions.get(session_id, [])
     chat_history.append(f"User: {input}")
     logger.info(f"Current chat history for session {session_id}: {chat_history}")
+
+    # 출력 형식 설정
     output_format = {"user_id": user_id, "baby_id": baby_id, "session_id": session_id}
 
-    # Invoke
+    # 에이전트 실행
     intermediate_steps = []
     agent_step = None
 
     while not isinstance(agent_step, AgentFinish):
+        # 에이전트 호출
         agent_step = agent.invoke(
             {
                 "input": f"user_id: {user_id}, baby_id: {baby_id}, input: {input}",
@@ -101,26 +81,54 @@ async def process_user_query(query: Query):
         )
 
         if isinstance(agent_step, AgentAction):
+            # 도구 실행
             tool_name = agent_step.tool
             tool_input = agent_step.tool_input
             tool_to_use = find_tool(tools, tool_name)
             observation = tool_to_use.invoke(tool_input)
             print(f"{observation=}")
 
+            # retriever_assistant가 결과를 찾지 못한 경우 처리
+            if tool_name == "retriever_assistant" and observation == "No results found":
+                except_tool = find_tool(tools, "except_situation_assistant")
+                except_input = json.dumps(
+                    {
+                        "query": input,
+                        "thought": "retriever_assistant found no results, using except_situation_assistant",
+                    }
+                )
+                observation = except_tool.invoke(except_input)
+                agent_step = AgentFinish(
+                    return_values={"output": observation},
+                    log="Used except_situation_assistant due to no results from retriever_assistant.",
+                )
+            # 특정 도구들의 경우 바로 에이전트 종료
+            elif tool_name in [
+                "write_diary_assistant",
+                "sharing_assistant",
+                "except_situation_assistant",
+            ]:
+                agent_step = AgentFinish(
+                    return_values={"output": observation},
+                    log=f"{tool_name} tool returned {observation}, ending the agent execution.",
+                )
+
             intermediate_steps.append((agent_step, str(observation)))
 
         if isinstance(agent_step, AgentFinish) or agent_step:
             result = agent_step
 
+    # 결과 처리
     if output_format.get("response") is None:
         print("=== Agent Finish!!===")
         print(f"{result=}")
         output_format.update({"response": result.return_values["output"]})
 
-    # Save chat history
+    # 채팅 기록 저장
     chat_history.append(f"Bot: {output_format['response']}")
     user_sessions[session_id] = chat_history
     return output_format
 
+    # 예외 처리
     # except Exception as e:
     #     raise HTTPException(status_code=500, detail=str(e))
--- app/api/daysummary/utils/agent_util.py
+++ app/api/daysummary/utils/agent_util.py
@@ -1,6 +1,53 @@
 from typing import List
 from langchain.tools import Tool
+import os
+from typing import Union
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools.render import render_text_description
+from langchain_openai import ChatOpenAI
 
+
+def setup_agent(tools: List[str]):
+    # env setting
+    llm_model = os.getenv("LLM_MODEL")
+    openai_key = os.getenv("OPENAI_API_KEY")
+
+    # template load
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    prompts_dir = os.path.join(
+        parent_dir, "prompts", "agent", "daysummary_react_prompt.txt"
+    )
+
+    with open(prompts_dir, "r") as f:
+        template = f.read()
+
+    # Prompt setting
+    prompt = PromptTemplate.from_template(template)
+    prompt = prompt.partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join(t.name for t in tools),
+    )
+
+    # llm setting
+    llm = ChatOpenAI(
+        model=llm_model,
+        temperature=0,
+        openai_api_key=openai_key,
+        stop=["\nObservation", "Observation"],
+    )
+
+    # agent setting
+    agent: Union[AgentAction, AgentFinish] = (
+        prompt | llm | ReActSingleInputOutputParser()
+    )
+
+    return agent
+
+
+# Agent가 사용할 도구 이름 찾는 함수
 def find_tool(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
